78095b0361a9ed15dd76156ed780594d767b5e86
lpile <logan.pile@gmail.com>
2019-04-29 21:26:37 -0600

    Add Spec with validations for editing an article

---
+++ spec/features/user_edits_an_article_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe "user edits an article" do
+  describe "they link from the show page" do
+    describe "they fill in an edit field and submit" do
+      it "displays the updated information on the show page" do
+      article = Article.create!(title: "New Title!", body: "New Body!")
+
+      visit article_path(article)
+      click_link "Edit"
+
+      fill_in "article[title]", with: "Different Title!"
+      fill_in "article[body]", with: "Different Body!"
+      click_on "Update Article"
+
+      expect(page).to have_content("Different Title!")
+      expect(page).to have_content("Different Body!")
+      end
+    end
+  end
+end
