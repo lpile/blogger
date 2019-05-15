00365e7606ca40ab7c6ebeee56fa9999d7ea8ab0
lpile <logan.pile@gmail.com>
2019-05-15 15:17:52 -0600

    Add timestamps to comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190515210844) do
+ActiveRecord::Schema.define(version: 20190515211547) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 20190515210844) do
     t.string "author_name"
     t.text "body"
     t.bigint "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
